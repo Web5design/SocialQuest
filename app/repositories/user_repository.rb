87a36f5d3669368bb011bde8e65af0399c243d96
Gregor Robert Russbuelt <me@russbuelt.com>
2012-08-22 16:35:16 +0200

    user_repository#find

--- app/repositories/user_repository.rb
+++ app/repositories/user_repository.rb
@@ -1,4 +1,11 @@
 # encoding: UTF-8
 
 class UserRepository
+  class << self
+    def find(id)
+      [true, User.find(id)]
+    rescue
+      [false, nil]
+    end
+  end
 end
--- spec/repositories/user_repository_spec.rb
+++ spec/repositories/user_repository_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe UserRepository do
+  describe "#find" do
+    it "returns true and the user when user is found" do
+      user = double("user")
+      User.stub(:find).with(1) { user }
+      subject = UserRepository.find(1) 
+      subject[0].should == true
+      subject[1].should == user
+    end
+    
+    it "returns false and nil when no user is found" do
+      subject = UserRepository.find(1) 
+      subject[0].should == false
+      subject[1].should == nil
+    end
+  end
 end
