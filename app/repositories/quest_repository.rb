2df1a1bec138cbe9e1388621a2889cf459626c5f
Gregor Robert Russbuelt <me@russbuelt.com>
2012-08-22 19:06:25 +0200

    abstract definition of quest_repository

--- app/repositories/quest_repository.rb
+++ app/repositories/quest_repository.rb
@@ -1,4 +1,14 @@
 # encoding: UTF-8
 
 class QuestRepository
+  ## Abstract
+  # class << self
+  #   def all
+  #     Quest.all
+  #   end
+  #   
+  #   def by_user(user)
+  #     user.quests
+  #   end
+  # end
 end
