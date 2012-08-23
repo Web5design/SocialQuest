2d5565b696e5b5ab24e4e10493b1eefd4de38628
Vikas <vikas.narula@gmail.com>
2012-08-24 01:00:29 +0200

    fix errors in quest.rb

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -3,12 +3,11 @@ class Quest < ActiveRecord::Base
   
 	# TODO belongs_to :owner 
   belongs_to :user
-<<<<<<< HEAD
   has_one :type
   has_many :skills
   
   validates_presence_of :user_id
-  attr_accessible :title, :description, :type_id, :start_time, :end_time, :user_id
+  attr_accessible :title, :description, :type_id, :start_time, :end_time, :user_id, :latitude, :longitude
   
   api_accessible :public do |template|
     template.add :title
@@ -21,12 +20,4 @@ class Quest < ActiveRecord::Base
     template.add :created_at
     template.add :updated_at
   end
-=======
-
-  # TODO: This is for the change in the table
-  # has_one :type
-  
-  validates_presence_of :user_id
-  attr_accessible :title, :description, :type_id, :start_time, :end_time, :user_id, :latitude, :longitude
->>>>>>> afa2709c00d6ce5b54c122ae37887827447e4bad
 end
