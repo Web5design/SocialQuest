26faf8e6172fc941a4524f00cf59ae2d27cf0357
Patrick Kaddu <admin@hack2learn.org>
2012-08-24 11:52:49 +0200

    added missing end to base controller

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,5 +3,6 @@ class BaseController < ApplicationController
   	respond_to do |format|
     	format.html { redirect_to root }
     	format.json { render json: { controller: self.class.inspect } }
+    end
   end
 end
